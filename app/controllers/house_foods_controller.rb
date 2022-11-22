44117aa64fd658d151fd7a22836b61b07275a4e7
Sho Fujiwara <shofujiwara505@gmail.com>
2022-11-22 17:29:21 +0900

    house food new

--- app/controllers/house_foods_controller.rb
+++ app/controllers/house_foods_controller.rb
@@ -1,16 +1,9 @@
 class HouseFoodsController < ApplicationController
   def index
-<<<<<<< HEAD
     @house_foods = policy_scope(HouseFood)
     @foods = HouseFood.all
     @house = House.find(2)
-    # Changed it to 2 to test out something can return to 1 if you want
-=======
-    # pundit thing
     @foods = policy_scope(HouseFood)
-    # authorize @foods
-
->>>>>>> 08ecd8a5c380339bb11924b5e58c32b6771996ac
   end
 
   def show
@@ -32,7 +25,6 @@ class HouseFoodsController < ApplicationController
     if @house_food.save
       redirect_to house_foods_path
     else
-      raise
       render :new, status: :unprocessable_entity
     end
   end
@@ -43,13 +35,9 @@ class HouseFoodsController < ApplicationController
   def destroy
   end
 
-<<<<<<< HEAD
   private
 
   def house_food_params
     params.require(:house_food).permit( :food_id, :amount, :photo, :bought_date, :expiry_date)
   end
-
-=======
->>>>>>> 08ecd8a5c380339bb11924b5e58c32b6771996ac
 end
