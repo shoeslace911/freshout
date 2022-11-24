27919a0982b6cfb37eb2f8c78d8185f4a0a294c0
sunny liu <372930@gmail.com>
2022-11-24 10:54:55 +0900

    add a shopping list show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :shopping_lists, only: [:show]
   resources :house_foods do
     member do
       patch :eat
