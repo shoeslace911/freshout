b60cb1de54ca25bf3ef54fdc878c407417911072
Will Hargrave <77623750+Willhargrave@users.noreply.github.com>
2022-11-28 17:40:53 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   has_many :items
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :services
-  has_many :notification, as: :recipient
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
