68694554d81647bebe493229897c4abcfc563ed7
sunny liu <372930@gmail.com>
2022-11-28 17:01:07 +0900

    adjust the measurement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 require "open-uri"
 require "faker"
 
+Item.destroy_all
+HouseFood.destroy_all
+User.destroy_all
+House.destroy_all
+ShoppingList.destroy_all
+
 House.create!(
   name: "#{Faker::Name.last_name}'s house"
 )
@@ -13,13 +19,13 @@ puts "Shopping list created"
 
 10.times do
   foods = Food.all.uniq.sample
-  HouseFood.where(food: foods).find_or_create_by(
+  HouseFood.create!(
     food: foods,
     house: House.first,
     bought_date: Faker::Date.between(from: '2023-01-01', to: '2023-01-04'),
     expiry_date: Faker::Date.between(from: '2023-01-04', to: '2023-01-08'),
     comment: '',
-    measurement: "",
+    measurement: "bag",
     amount: rand(1..4),
     owned: true
   )
@@ -27,18 +33,19 @@ puts "Shopping list created"
   # puts "Created #{house_food.name} as a house food."
 end
 puts "House foods created"
+User.create!(username: 'will_hargrave', email: 'will@will.com', password: 123456, house: House.first)
+User.create!(username: 'sho_fujiwara', email: 'sho@sho.com', password: 123456, house: House.first)
 
 6.times do
   Item.create!(
     shopping_list: ShoppingList.first,
     food: Food.all.sample,
-    amount: 6
+    amount: 6,
+    user: User.all.sample
   )
 end
 puts "Items created"
 
-User.create!(username: 'will_hargrave', email: 'will@will.com', password: 123456, house: House.first)
-User.create!(username: 'sho_fujiwara', email: 'sho@sho.com', password: 123456, house: House.first)
 puts "Users created"
 
 puts "Created #{Food.count} foods, #{House.count} houses, #{HouseFood.count} house foods, #{User.count} users, #{ShoppingList.count} shopping list, #{Item.count} items."
